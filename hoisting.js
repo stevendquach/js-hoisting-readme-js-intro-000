534a42120f73b62966e25e1c67ca170e88be6fb5
Steven Quach <stevendquach@gmail.com>
2020-08-18 08:15:51 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,12 +10,13 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
+ 
 
   var thisIsCrazy = function (){
-    console.log("hey!!!")
+    console.log("hey!!!") 
   }
 }
-crazy();
+crazy()
 
 function sayMyName() {
   // fix the code in here:
